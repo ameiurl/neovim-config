0e6611aa3085b2330f44d8576b2be8abb077a8b7
amei <cms1984@msn.com>
2023-11-13 17:47:57 +0800

    schemecolor

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -125,8 +125,9 @@ local plugins = {
   					vim.cmd [[
                         let g:seoul256_background = 236
     					colorscheme seoul256 
-	  					hi phpVarSelector    guifg=#D9D9D9              gui=none
-	  					hi phpIdentifier    guifg=#D9D9D9              gui=none
+	  					hi phpVarSelector    guifg=#FFBFBD              gui=none
+	  					"hi phpVarSelector    guifg=#D9D9D9              gui=none
+	  					"hi phpIdentifier    guifg=#D9D9D9              gui=none
                         hi phpStringSingle    guifg=#BCDDBD              gui=none
                         hi phpStringDouble    guifg=#BCDDBD              gui=none
 	  					hi phpFunctions    guifg=#e2c792              gui=none
@@ -144,8 +145,8 @@ local plugins = {
 			{ 'sainnhe/everforest',                          -- colorschemes 
                 config = function()
   					vim.cmd [[
-                        "let g:everforest_background = 'hard'
                         "let g:everforest_better_performance = 1
+                        "let g:everforest_background = 'soft'
     					"colorscheme everforest 
   					]]
 				end},
