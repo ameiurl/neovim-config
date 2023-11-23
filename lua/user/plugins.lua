be8d5c697198a4965d511b0bf4de06c65eaf89a0
amei <cms1984@msn.com>
2023-11-23 18:04:32 +0800

    schemecolor

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -100,22 +100,22 @@ local plugins = {
 
 	quality_of_life = {
 		themeing = {
-			{ 'shaunsingh/seoul256.nvim',                          -- colorschemes 
+			{ 'ameiurl/seoul256.vim',                          -- colorschemes 
                 config = function()
   					vim.cmd [[
     					colorscheme seoul256 
-	  					hi phpVarSelector    guifg=#FFBFBD              gui=none
-                        hi phpStringSingle    guifg=#BCDDBD              gui=none
-                        hi phpStringDouble    guifg=#BCDDBD              gui=none
-	  					hi phpFunctions    guifg=#e2c792              gui=none
-	  					hi phpMethods    guifg=#e2c792              gui=none
-	  					hi phpSpecialFunction    guifg=#e2c792              gui=none
-	  					hi phpBaselib    guifg=#e2c792              gui=none
-	  					hi phpNumber    guifg=#e55561              gui=none
-	  					hi phpFloat    guifg=#e55561              gui=none
-                        hi htmlTag    guifg=#98BC99              gui=none
-                        hi htmlEndTag    guifg=#98BC99              gui=none
-                        hi javaScript    guifg=#C8C8C8              gui=none
+	  					hi phpVarSelector       guifg=#FFBFBD              gui=none
+                        hi phpStringSingle      guifg=#BCDDBD              gui=none
+                        hi phpStringDouble      guifg=#BCDDBD              gui=none
+	  					hi phpFunctions         guifg=#e2c792              gui=none
+	  					hi phpMethods           guifg=#e2c792              gui=none
+	  					hi phpSpecialFunction   guifg=#e2c792              gui=none
+	  					hi phpBaselib           guifg=#e2c792              gui=none
+	  					hi phpNumber            guifg=#e55561              gui=none
+	  					hi phpFloat             guifg=#e55561              gui=none
+                        hi htmlTag              guifg=#98BC99              gui=none
+                        hi htmlEndTag           guifg=#98BC99              gui=none
+                        hi javaScript           guifg=#C8C8C8              gui=none
   					]]
 				end,
             },
