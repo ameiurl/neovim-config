df8fd8b508785f05ac8475cf7eab2aed99fb718b
amei <cms1984@msn.com>
2025-05-26 23:52:21 +0000

    rm avante

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -7,4 +7,12 @@ return {
 			vim.g.instant_markdown_autostart = 0
 		end,
 	},
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante", "codecompanion" },
+      },
+      ft = { "markdown", "Avante", "codecompanion" },
+    },
 }
