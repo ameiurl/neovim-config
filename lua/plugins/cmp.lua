33a7e454d3d10751033302ce4ae5a5afc1c724db
amei <cms1984@msn.com>
2025-06-01 22:51:14 +0000

    add gitignore

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -121,7 +121,10 @@ return {
                 ['<Tab>'] = cmp.mapping(function(fallback)
 
                     local has_words_before = function()
-                        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                        -- local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                        local cursor = vim.api.nvim_win_get_cursor(0)
+                        local line = cursor[1]
+                        local col = cursor[2]
                         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
                     end
 
