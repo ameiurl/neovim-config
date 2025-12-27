7cd6ef097c2ec6c8f8aee29c2ac5ebd593278610
amei <cms1984@msn.com>
2025-12-27 19:21:36 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -224,3 +224,15 @@ vim.api.nvim_create_autocmd('User', {
         vim.b.copilot_suggestion_hidden = false
     end,
 })
+
+-- show cursor line only in active window
+local cursorGrp = vim.api.nvim_create_augroup("CursorLine", { clear = true })
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+    pattern = "*",
+    command = "set cursorline",
+    group = cursorGrp,
+})
+vim.api.nvim_create_autocmd(
+    { "InsertEnter", "WinLeave" },
+    { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
