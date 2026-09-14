e418c2e0cdfe7662daffef4aeabb4aa39ac5fc81
chen <cms1984@msn.com>
2026-09-14 16:39:53 +0800

    change: 注释掉同步文件显示

--- lua/plugins/multisync.lua
+++ lua/plugins/multisync.lua
@@ -1,12 +1,12 @@
 -- 本地插件：右下角 fidget 风格浮窗，实时 tail /home/amei/multi_sync.log
 -- 可在下方 setup({ ... }) 中调整 interval_ms / max_lines / max_width 等，见文件头注释
-return {
-    {
-        dir = vim.fn.stdpath('config') .. '/multisync-status',
-        name = 'multisync-status',
-        event = 'VeryLazy',
-        config = function()
-            require('multisync-status').setup({})
-        end,
-    },
-}
+-- return {
+--     {
+--         dir = vim.fn.stdpath('config') .. '/multisync-status',
+--         name = 'multisync-status',
+--         event = 'VeryLazy',
+--         config = function()
+--             require('multisync-status').setup({})
+--         end,
+--     },
+-- }
